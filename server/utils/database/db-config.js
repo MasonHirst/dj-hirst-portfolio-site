cef65691601f986b8622bd785223309c9bf13a71
Mason Hirst <mason.j.hirst@gmail.com>
2024-09-24 19:03:59 -0600

    configured the app to run successfully on fly.io

--- server/utils/database/db-config.js
+++ server/utils/database/db-config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
-const { DATABASE_CONNECTION_STRING, NODE_ENV } = process.env
+const { DATABASE_CONNECTION_STRING } = process.env
 
 const db = new Sequelize(DATABASE_CONNECTION_STRING, {
   dialect: 'postgres',
